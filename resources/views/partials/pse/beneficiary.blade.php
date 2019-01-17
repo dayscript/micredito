42f41325c07dfd87b5a1690349f998d8f59edbbb
aacevedoday <aacevedo@dayscript.com>
2019-01-17 16:37:10 -0500

    update

--- resources/views/partials/pse/beneficiary.blade.php
+++ resources/views/partials/pse/beneficiary.blade.php
@@ -183,26 +183,32 @@
                         </div>
 
                         <ul class="nav nav-tabs" id="myTab" role="tablist">
-                          <li class="nav-item active">
-                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">RESUMEN</a>
+                          <li class="nav-item">
+                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">EXTRACTO</a>
+                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">MOVIMIENTO</a>
+                            <a class="nav-link" id="messages-tab" data-toggle="tab" href="#messages" role="tab" aria-controls="messages" aria-selected="false">Messages</a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" id="plan-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="plan" aria-selected="false">PLAN DE PAGOS</a>
+                            <a class="nav-link" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Settings</a>
                           </li>
                         </ul>
-                        <div class="tab-content" id="myTabContent">
-                          <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">asd</div>
-                          <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">dfg</div>
-                          <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">ñlk</div>
-                          <div class="tab-pane fade" id="plan" role="tabpanel" aria-labelledby="plan-tab">ñlk</div>
 
+                        <div class="tab-content">
+                          <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+                          <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                          <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab">...</div>
+                          <div class="tab-pane" id="settings" role="tabpanel" aria-labelledby="settings-tab">...</div>
                         </div>
+
+                        <script>
+                          $(function () {
+                            $('#myTab li:last-child a').tab('show')
+                          })
+                        </script>
 
                       </div>
                     </div>
