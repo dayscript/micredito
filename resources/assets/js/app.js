70db3c92c0229684e4eaf26cb472d5ded461ebf1
aacevedoday <you@example.com>
2019-01-19 20:20:23 -0500

    update

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,7 +10,7 @@ require('./bootstrap');
 
 
 window.Vue = require('vue');
-
+global.$ = global.jQuery = require('jquery');
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
